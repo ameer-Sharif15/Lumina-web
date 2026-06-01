74c526046f423767f58172e9d5dbf708a9ae5aab
Hafeez Abubakar <hafeezabubakar15@gmail.com>
2026-06-01 01:48:29 +0100

    working

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -41,7 +41,7 @@ export default function ContactForm() {
         </p>
       )}
 
-      <div className='grid grid-cols1  md:grid-cols-2 gap-4'>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
         <div>
           <label
             htmlFor='contact-name'
